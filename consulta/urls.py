bdfe5df8bcec18432b4a2daa83badd6888e91aef
Pedro <pedrohazevedo@gmail.com>
2023-11-05 01:06:57 -0300

    1

--- consulta/urls.py
+++ consulta/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from consulta.views import index, SearchView
 
 urlpatterns = [
-    path('', index),
+    path('', index, name='index'),
     path('search', SearchView.as_view()),
     ]
--- consulta/views.py
+++ consulta/views.py
@@ -1,15 +1,47 @@
 from django.shortcuts import render
 from consulta.models import Livros, Estoque
 from django.views.generic.list import ListView
+from openpyxl import load_workbook
+
 
 def index(request):
-    query = Livros.objects.get(nbook = 'pa1649')
+    if "GET" == request.method:
+        return render(request,'index.html')
+    if "POST" == request.method:
+        filename = request.FILES['excel_file']
+        wb = load_workbook(request.FILES['excel_file'])
+        ws = wb.active
+        dict_isbn = {}
+        for row in ws['a']:
+            if ws[f'A{row.row}'].value == 'ISBN':
+                pass
 
-    context = {'query': query
+            else:
+                isbn = ws[f'A{row.row}'].value
+                if str(isbn) in dict_isbn:
+                    dict_isbn[f'{isbn}'] = int(dict_isbn[f'{isbn}']) + int(ws[f'D{row.row}'].value)
 
-    }
+                else:
+                    dict_isbn[f'{isbn}'] = int(ws[f'D{row.row}'].value)
+            for item in dict_isbn:
+                isbn = item
+                qtd = dict_isbn[item]
+                queryisbn = Livros.objects.get(isbn1=isbn)
+                queryestoque = Estoque.objects.filter(nbook = queryisbn.nbook).first()
+                estoque = queryestoque.disp
+                estoque_ff = queryestoque.disp_ff
+                if estoque >= qtd:
+                    ws[f'B{row.row}'] = 'Circulo'
 
-    return render(request, 'index.html', context)
+                elif estoque < qtd > 0:
+                    estoque = estoque_ff + estoque
+                    ws[f'B{row.row}'] = 'Catavento'
+                else:
+                    ws[f'B{row.row}'] = ''
+            wb.save(f'{filename}')
+
+        return render(request, 'index.html')
+
 
 class SearchView(ListView):
     model = Livros
